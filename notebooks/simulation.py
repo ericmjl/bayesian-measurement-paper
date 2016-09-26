9fa60f3de02c06ae4bbf2fe32d65d9c399f3a16e
Eric J. Ma <ericmajinglong@gmail.com>
2016-09-26 18:16:25 -0400

    bug fix

--- notebooks/simulation.py
+++ notebooks/simulation.py
@@ -26,4 +26,4 @@ sge_header = """
 # Write master script
 with open('sim_results/master.sh', 'w') as f:
     f.write(sge_header)
-    f.write('python model.py --max_n_reps=21 --n_sims=20\n'.format(n))
+    f.write('python model.py --max_n_reps=21 --n_sims=20\n')
