ffa5086c12b9340585f49dd5ae54006b58854e9f
Eric J. Ma <ericmajinglong@gmail.com>
2016-09-29 04:15:51 -0400

    added template back to bash script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ pandoc paper.md -o paper.pdf \
     --filter pandoc-fignos \
     --filter pandoc-tablenos \
     --filter pandoc-citeproc \
+    --template default.latex \
     --bibliography "../references-master/papers-library.bib" \
 
 open paper.pdf
