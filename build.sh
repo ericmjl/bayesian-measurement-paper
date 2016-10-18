c0dee5c4f2e991f209ed1567cd34460eff278612
Eric J. Ma <ericmajinglong@gmail.com>
2016-10-18 08:18:53 -0400

    updated build script to use xelatex

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pandoc paper.md -o paper.pdf \
     --filter pandoc-tablenos \
     --filter pandoc-citeproc \
     --template default.latex \
+    --latex-engine=xelatex \
     --bibliography "../references-master/papers-library.bib" \
 
 open paper.pdf
